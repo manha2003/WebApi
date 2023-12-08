75419a9ceec6543e8d396a5894bf01f23e7e6e64
manha2003 <manha10122003@gmail.com>
2023-12-08 10:05:05 +0700

    update group 1

---
+++ validation-gr1-asm/README.md
@@ -0,0 +1,3 @@
+﻿# Validation data
+Validation-featureV1: Used data annotation default [Required].
+Validation-featureV1: Handling Validation Errors
---
+++ validation-gr1-asm/Validation-featureV1/Student.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebAPISample
+{
+    public class Student
+    {
+        public Student(int id, string name, string gradeId) 
+        {
+            Id = id;
+            Name = name;
+            GradeId = gradeId;
+        }
+
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Name is required")]
+        [StringLength(50, ErrorMessage = "Name cannot be longer than 50 characters")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "GradeId is required")]
+        public string GradeId { get; set; }
+    }
+}
